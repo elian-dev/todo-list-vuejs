425754aac1c5d8d3e8300f9ff4d802c2c422bbd6
elian-dev <geordymemdoza@gmail.com>
2023-10-29 02:02:49 -0600

    feat: :sparkles: Add responsive layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,53 @@
 <script setup>
-  import { ref, onMounted, computed, watch } from 'vue';
-  import GreetingSection from './components/GreetingSection.vue';
-  import CreateTodo from './components/CreateTodo.vue';
-  import TodoList from './components/TodoList.vue';
+import { ref, onMounted, computed, watch } from "vue";
+import GreetingSection from "./components/GreetingSection.vue";
+import CreateTodo from "./components/CreateTodo.vue";
+import TodoList from "./components/TodoList.vue";
 
-  const todos = ref([]);
-  const isEmpty = ref(true);
+const todos = ref([]);
+const isEmpty = ref(true);
 
-  watch(todos, newVal => {
-    localStorage.setItem('todos', JSON.stringify(newVal));
-    isEmpty.value =  todos.value.length === 0;
-  }, {deep: true});
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+    isEmpty.value = todos.value.length === 0;
+  },
+  { deep: true }
+);
 
-  onMounted(() => {
-    todos.value = JSON.parse(localStorage.getItem('todos') || []);
-    isEmpty.value =  todos.value.length === 0;
-  });
+onMounted(() => {
+  todos.value = JSON.parse(localStorage.getItem("todos") || []);
+  isEmpty.value = todos.value.length === 0;
+});
 
-  // Event to remove todo
-  const removeTodo = todo => {
-    todos.value = todos.value.filter(t => t !== todo);
-  }
+// Event to remove todo
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
 
-  // Event to remove all todos
-  const removeAllTodos = () => {
-    todos.value = [];
-  }
-
+// Event to remove all todos
+const removeAllTodos = () => {
+  todos.value = [];
+};
 </script>
 
 <template>
   <main class="app">
-
     <GreetingSection />
 
-    <CreateTodo :todos="todos" />
-
-    <TodoList 
-      :todos="todos" 
-      :isEmpty="isEmpty" 
-      @remove="removeTodo" 
-      @removeAll="removeAllTodos"
-    />
-
+    <div class="row">
+      <div class="col">
+        <CreateTodo :todos="todos" />
+      </div>
+      <div class="col">
+        <TodoList
+          :todos="todos"
+          :isEmpty="isEmpty"
+          @remove="removeTodo"
+          @removeAll="removeAllTodos"
+        />
+      </div>
+    </div>
   </main>
 </template>
--- src/components/CreateTodo.vue
+++ src/components/CreateTodo.vue
@@ -1,72 +1,70 @@
 <script setup>
-    import { ref } from 'vue';
+import { ref } from "vue";
 
-    const props = defineProps(['todos']);
+const props = defineProps(["todos"]);
 
-    const input_content = ref('');
-    const input_category = ref(null);
+const input_content = ref("");
+const input_category = ref(null);
 
-    const addTodo = () => {
-        if(input_content.value.trim() === '' || input_category.value === null) {
-            return
-        };
+const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) {
+    return;
+  }
 
-        props.todos.push({
-            content: input_content.value.trim(),
-            category: input_category.value,
-            done: false,
-            createdAt: new Date().getTime()
-        })
+  props.todos.push({
+    content: input_content.value.trim(),
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(),
+  });
 
-        input_content.value = ''
-        input_category.value = null
-    };
-
+  input_content.value = "";
+  input_category.value = null;
+};
 </script>
 
-<template> 
-    <section class="create-todo">
-      <h3>CREATE A TODO</h3>
+<template>
+  <section class="create-todo">
+    <h3>CREATE A TODO</h3>
 
-      <form @submit.prevent="addTodo">
-        <h4>What's on your todo list?</h4>
-        
-        <input 
-          type="text" 
-          placeholder="e.g. Make a video" 
-          v-model="input_content"
-          name="todo-name"
-          id="todo-name"
-        />
+    <form class="form" @submit.prevent="addTodo">
+      <h4>What's on your todo list?</h4>
 
-        <h4>Pick a category</h4>
+      <input
+        type="text"
+        placeholder="e.g. Make a video"
+        v-model="input_content"
+        name="todo-name"
+        id="todo-name"
+      />
 
-        <article class="options">
-          <label>
-            <input 
-              type="radio"
-              name="category"
-              value="business"
-              v-model="input_category"
-            />
-            <span class="bubble business"></span>
-            <div>Business</div>
-          </label>
+      <h4>Pick a category</h4>
 
-          <label>
-            <input 
-              type="radio"
-              name="category"
-              value="personal"
-              v-model="input_category"
-            />
-            <span class="bubble personal"></span>
-            <div>Personal</div>
-          </label>
-        </article>
+      <article class="options">
+        <label>
+          <input
+            type="radio"
+            name="category"
+            value="business"
+            v-model="input_category"
+          />
+          <span class="bubble business"></span>
+          <div>Business</div>
+        </label>
 
-        <input type="submit" name="create-todo" value="Add todo">
-      </form>
+        <label>
+          <input
+            type="radio"
+            name="category"
+            value="personal"
+            v-model="input_category"
+          />
+          <span class="bubble personal"></span>
+          <div>Personal</div>
+        </label>
+      </article>
 
-    </section>
+      <input type="submit" name="create-todo" value="Add todo" />
+    </form>
+  </section>
 </template>
